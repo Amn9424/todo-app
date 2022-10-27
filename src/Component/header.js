b629071b4a47e858b10e902f39ed1f85a34d3200
Aman Soni <soniaman093@gmail.com>
2022-10-27 21:43:05 +0530

    final

--- src/Component/header.js
+++ src/Component/header.js
@@ -1,11 +1,7 @@
 
 const Header = () => {
     return (
-<<<<<<< HEAD
-        <header className="py-4 mb-8 text-white text-center font-bold text-2xl sticky top-0 backdrop-blur-md  bg-gray-800/40">
-=======
         <header className="py-4 w-full mb-8 text-white text-center font-bold text-2xl backdrop-blur-lg bg-gray-800/40 sticky top-0">
->>>>>>> header
             Todo App
         </header>
     )
